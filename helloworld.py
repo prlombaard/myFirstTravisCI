e8b2a1cc471383932cd727c60318dc57da95f048
Rudolph Lombaard <prlombaard@gmail.com>
2018-03-21 18:16:27 +0200

    Add Shebang

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 # script helloworld.py
 
 def main():
+    from sys import version
+    print(f"Running:\n{version}")
     print("Hello World")
 
 if __name__ == "__main__":
